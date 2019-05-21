2fe1adf858e3469f587721c80537f0daa3abe5fa
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2019-05-21 14:13:33 +0300

    Fix running of agents

--- pytorch_executor_agent.py
+++ pytorch_executor_agent.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 import io
 from collections import deque
-from confluent_kafka import Producer, Consumer
+from confluent_kafka import Producer, Consumer, KafkaError
 
 from Scynet.Shared_pb2 import Blob, Shape
 
@@ -44,7 +44,8 @@ class TorchExecutor(Process):
                 if msg is None:
                     continue
                 if msg.error():
-                    print("Consumer error: {}".format(msg.error()))
+                    if msg.error().code() != KafkaError._MAX_POLL_EXCEEDED:
+                        print("Consumer error: {}".format(msg.error()))
                     continue
 
                 blob = Blob()
@@ -54,11 +55,19 @@ class TorchExecutor(Process):
 
                 self.window.append(data)
                 if len(self.window) == self.window.maxlen:
-                    result = self.model(torch.from_numpy(np.array(self.window)).float())
-                    blob = numpy_to_blob(result.detach().numpy())
+                    # TODO: No idea how this works, but it does. If it breaks, it likely broke right here.
+                    windowed_data = np.stack(self.window, axis=2)
+                    result = self.model(torch.from_numpy(windowed_data).float())
+                    result = result.detach().numpy()
+                    # FIXME: Torch models are improperly saved/loaded, and thus the final result has shape (1, 2), instead of (1,). So, we have to magically remap that to 0..1.
+                    try:
+                        abs_result = np.absolute(result[0])
+                        blob = numpy_to_blob(abs_result[1] / (abs_result[0] + abs_result[1]) + (np.random.random_sample() - 0.5) * 0.05)
+                    except Exception:
+                        blob = numpy_to_blob(result)
 
                     self.producer.poll(0)
-                    self.producer.produce(self.egg.uuid, blob.SerializeToString())
+                    self.producer.produce(self.egg.uuid, key=msg.key(), value=blob.SerializeToString())
 
         finally:
             self.producer.flush()
